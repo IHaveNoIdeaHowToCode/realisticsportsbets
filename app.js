114c9e03698718aed025896c3db1288fb5b6089c
Nikolas Clark <nac09@msn.com>
2025-05-28 15:10:34 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+// console.log("Hello world! I am Peppercorn Bing Bong!")
+
+// LAB DATA
+let bank = 100;
+
+const players = [
+  { teamNumber: 1, emoji: "🏃‍♂️", skill: 10, name: "D'Marcus Williums" },
+  { teamNumber: 1, emoji: "🤾‍♂️", skill: 30, name: "Tyroil Smoochie-Wallace" },
+  { teamNumber: 1, emoji: "🏇", skill: 88, name: "Jackmerius Tacktheratrix" },
+  {
+    teamNumber: 1,
+    emoji: "🏌️‍♀️",
+    skill: 15,
+    name: "Javaris Jamar Javarison-Lamar",
+  },
+  { teamNumber: 1, emoji: "🏋️‍♂️", skill: 77, name: "D'Pez Poopsie" },
+  { teamNumber: 1, emoji: "🏌️‍♂️", skill: 21, name: "D'Jasper Probincrux III" },
+  { teamNumber: 1, emoji: "🤾", skill: 5, name: "Leoz Maxwell Jilliumz" },
+  { teamNumber: 1, emoji: "🏂", skill: 99, name: "Hingle McCringleberry" },
+  { teamNumber: 1, emoji: "🧘‍♀️", skill: 50, name: "L'Carpetron Dookmarriot" },
+  { teamNumber: 1, emoji: "🚶‍♀️", skill: 1, name: "Xmus Jaxon Flaxon-Waxon" },
+  { teamNumber: 2, emoji: "🏋️‍♀️", skill: 61, name: "Saggitariutt Jefferspin" },
+  { teamNumber: 2, emoji: "🤺", skill: 34, name: "Quatro Quatro" },
+  { teamNumber: 2, emoji: "🏄", skill: 71, name: "X-Wing @Aliciousness" },
+  { teamNumber: 2, emoji: "🧜‍♂️", skill: 76, name: "Bisquiteen Trisket" },
+  {
+    teamNumber: 2,
+    emoji: "🤸",
+    skill: 47,
+    name: "Scoish Velociraptor Maloish",
+  },
+  { teamNumber: 2, emoji: "⛹️‍♀️", skill: 23, name: "Donkey Teeth" },
+  {
+    teamNumber: 2,
+    emoji: "🕴️",
+    skill: 58,
+    name: "T.J. A.J. R.J. Backslashinfourth V",
+  },
+  { teamNumber: 2, emoji: "💃", skill: 99, name: "Firstname Lastname" },
+  { teamNumber: 2, emoji: "🧍‍♂️", skill: 3, name: "Dan Smith" },
+  { teamNumber: 2, emoji: "🐅", skill: 100, name: "Tiger" },
+];
+
+// NOTE -- DRAW PLAYERS SECTION
+
+function drawTeam1() {
+  const team1Container = document.getElementById('teamOneRoster')
+  team1Content = "";
+
+  players.forEach((player) => {
+    if (player.teamNumber == 1) {
+      // console.log(player)
+      team1Content += `<span title="${player.name}">${player.emoji}</span>`
+    }
+  })
+  team1Container.innerHTML = team1Content
+}
+
+function drawTeam2() {
+  const team2Container = document.getElementById('teamTwoRoster')
+  team2Content = "";
+
+  players.forEach((player) => {
+    if (player.teamNumber == 2) {
+      // console.log(player)
+      team2Content += `<span title="${player.name}">${player.emoji}</span>`
+    }
+  })
+  team2Container.innerHTML = team2Content
+}
+
+drawTeam1()
+drawTeam2()
